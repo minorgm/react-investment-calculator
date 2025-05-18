53a917828f1ea6e5293e2e66010d567ebc419471
mgarcia <mgarcia@growthaccelerationpartners.com>
2025-05-18 17:52:50 +0200

    Adding header and user input components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import { CORE_CONCEPTS } from './data.js';
-import Header from './components/Header/Header.jsx';
-import CoreConcepts from './components/CoreConcept.jsx';
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <main>
-        <section id='core-concepts'>
-          <h2>Core Concepts</h2>
-          <ul>
+    <Header />
 
-          </ul>
-        </section>
-      </main>
-    </div>
   );
 }
 
