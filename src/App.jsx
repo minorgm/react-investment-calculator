97e5fdc98c0c4503bd818950bb1985d8d11105a0
mgarcia <mgarcia@growthaccelerationpartners.com>
2025-05-18 15:29:33 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header/Header.jsx';
+import CoreConcepts from './components/CoreConcept.jsx';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
