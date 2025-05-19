f59d8b8e85b8ed92d8694365b584261db0efb533
mgarcia <mgarcia@growthaccelerationpartners.com>
2025-05-19 13:37:49 +0200

    Adding results table

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,9 +1,30 @@
-import { calculateInvestmentResults } from "../util/investment.js"
+import { calculateInvestmentResults, formatter } from "../util/investment.js"
 
-export default function Results({ input}) {
+export default function Results({ input }) {
     const resultsData = calculateInvestmentResults(input);
 
     return (
-        <p>Results...</p>
+        <table id="result">
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Investment Value</th>
+                    <th>Interest (Year)</th>
+                    <th>Total Interest</th>
+                    <th>Invested Capital</th>
+                </tr>
+            </thead>
+            <tbody>
+                {resultsData.map((yearData) => (
+                    <tr key={yearData.year}>
+                        <td>{yearData.year}</td>
+                        <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                        <td>{formatter.format(yearData.interest)}</td>
+                        <td>{yearData.totalInterest}</td>
+                        <td>{yearData.investedCapital}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     )
 }
