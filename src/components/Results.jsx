9ed04fbbdbcf95970e469a020434bc9a5dc2c1e2
mgarcia <mgarcia@growthaccelerationpartners.com>
2025-05-19 12:04:46 +0200

    Computing values and properly handling number values

---
+++ src/components/Results.jsx
@@ -0,0 +1,9 @@
+import { calculateInvestmentResults } from "../util/investment.js"
+
+export default function Results({ input}) {
+    const resultsData = calculateInvestmentResults(input);
+
+    return (
+        <p>Results...</p>
+    )
+}
